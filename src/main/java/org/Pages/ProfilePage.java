0238ac1a01d23503e34726b5bdca0a8f0ede0e2e
UtkarshHowale <utkarshhowale16@gmail.com>
2023-12-29 21:05:27 +0530

    new changes

--- src/main/java/org/Pages/ProfilePage.java
+++ src/main/java/org/Pages/ProfilePage.java
@@ -1,6 +1,5 @@
 package org.Pages;
 
-import org.checkerframework.checker.index.qual.PolyUpperBound;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
